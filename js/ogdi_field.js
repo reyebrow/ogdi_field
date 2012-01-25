773bb8c2a310c5d86eb814e371ca7e99426daba4
Colin Calnan <colin@colincalnan.com>
2012-01-25 14:48:36 -0800

    Working on converting this to Drupal 7. Right now this module is functional as a field formatter for the Drupal 7 link field

--- js/ogdi_field.js
+++ js/ogdi_field.js
@@ -1,53 +1,58 @@
 // $Id$
-
-//==========================================//
-// BUILDING THE TABS
-//==========================================//
-Drupal.behaviors.buildTabs = function() {
-  if($('#ogdi-wrapper').length > 0) {
-    // Build the Map - this stuff must come first..
-    var container = $('#ogdi-map #bing-map'); // Get the map container...
-    var url = container.text(); // Get the URL of the kml document...
-    var map;  // Set a map variable...
-    map = new VEMap('bing-map');  // Create a new map in the container <div id="bing-map">
-    map.LoadMap();  // Load the map...
-    var shapeLayer = new VEShapeLayer();  // Create a shape layer to import the KML data into...
-    var shapeSpec = new VEShapeSourceSpecification(VEDataType.ImportXML, url, shapeLayer);
-    map.ImportShapeLayerData(shapeSpec);  
+(function ($) {
+  //==========================================//
+  // BUILDING THE TABS
+  //==========================================//
+  Drupal.behaviors.buildTabs = {
+    attach: function (context, settings) {
+      if($('#ogdi-wrapper').length > 0) {
+        // Build the Map - this stuff must come first..
+        var container = $('#ogdi-map #bing-map'); // Get the map container...
+        var url = container.text(); // Get the URL of the kml document...
+        var map;  // Set a map variable...
+        map = new VEMap('bing-map');  // Create a new map in the container <div id="bing-map">
+        map.LoadMap();  // Load the map...
+        var shapeLayer = new VEShapeLayer();  // Create a shape layer to import the KML data into...
+        var shapeSpec = new VEShapeSourceSpecification(VEDataType.ImportXML, url, shapeLayer);
+        map.ImportShapeLayerData(shapeSpec);
+      
+        // Create the tabs...   
+        $("#ogdi-tabs").tabs();
+        
+        // Build the datagrid...
+        $('#ogdi-datagrid').dataTable({
+          "bJQueryUI": true,  // Add support for jQueryUI...
+          "bAutoWidth": true, // Allow it's width to be automatic...
+          "sPaginationType": "full_numbers",  // Use full numbers for pagination...
+          "sScrollX": "100%", // Provide a horizontal scrollbar...
+          "bScrollCollapse": true
+        });
+      }
+    }
+  }
   
-    // Create the tabs...   
-    $("#ogdi-tabs").tabs();
-    
-    // Build the datagrid...
-    $('#ogdi-datagrid').dataTable({
-      "bJQueryUI": true,  // Add support for jQueryUI...
-      "bAutoWidth": true, // Allow it's width to be automatic...
-      "sPaginationType": "full_numbers",  // Use full numbers for pagination...
-      "sScrollX": "100%", // Provide a horizontal scrollbar...
-      "bScrollCollapse": true
-    });
+  //==========================================//
+  // CREATING THE DIALOG BOX
+  //==========================================//
+  Drupal.behaviors.dialog = {
+    attach: function (context, settings) {
+      // If the dialog text exists...
+      if($('div.ogdi-helptext').length > 0) {
+        // Set a variable to use later...
+        var $dialog = $('div.ogdi-helptext');
+        // Hide the container...
+        $dialog.hide();
+        // Get the tooltip link...
+        var $tooltip = $('a.ogdi-helptip');
+        // On click
+        $tooltip.click(function() {
+          $dialog.clone().dialog({  // Clone the dialog container to prevent the dialog() function from eating it...
+            width: 680,
+            modal: true
+          });
+          return false;
+        });
+      }
+    }
   }
-}
-
-//==========================================//
-// CREATING THE DIALOG BOX
-//==========================================//
-Drupal.behaviors.dialog = function(context) {
-  // If the dialog text exists...
-  if($('div.ogdi-helptext').length > 0) {
-    // Set a variable to use later...
-    var $dialog = $('div.ogdi-helptext');
-    // Hide the container...
-    $dialog.hide();
-    // Get the tooltip link...
-    var $tooltip = $('a.ogdi-helptip');
-    // On click
-    $tooltip.click(function() {
-      $dialog.clone().dialog({  // Clone the dialog container to prevent the dialog() function from eating it...
-        width: 680,
-        modal: true
-      });
-      return false;
-    });
-  }
-}
+})(jQuery);
